6e951703f30a0c097c09c2dd2dfbb6cc2593e7f2
Leo VUYLSTEKER <vuylstel@meleze17.iut-infobio.priv.univ-lille1.fr>
2015-09-21 14:22:31 +0200

    2eme version

--- src/Fenetre.java
+++ src/Fenetre.java
@@ -1,5 +1,9 @@
 import java.awt.Color;
 import java.awt.Label;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Random;
+import java.util.Scanner;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -7,15 +11,21 @@ import javax.swing.JTextField;
 
 
 public class Fenetre extends JFrame{
-
 	private JTextField monTexte;
-
+	private int nbAlea;
+	private Label affichage = new Label("Veuillez saisir un nombre entre 1 et 100");
+	
 	public Fenetre(){
-
 		this.setTitle("Nombre magique : ");
 		this.setSize(400, 400);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Scanner sc = new Scanner(System.in);
+		Random r = new Random();
+		
+		nbAlea = 1 + r.nextInt(100 - 1);
+		boolean victoire = false;
 
 
 		JPanel pan = new JPanel();
@@ -23,20 +33,59 @@ public class Fenetre extends JFrame{
 		this.setContentPane(pan);
 
 		//Label
-		Label monLab = new Label("Bonjour");
-		pan.add(monLab);
+		pan.add(affichage);
 
 
 		// Création du JTextField
 		JTextField texteSaisi = new JTextField();
+		texteSaisi.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getExtendedKeyCode() == KeyEvent.VK_ENTER){
+					try{
+						String s = calcul(Integer.parseInt(monTexte.getText()));
+						affichage.setText(s);
+						monTexte.setText("");
+						
+						
+					} catch(Exception ex){
+						affichage.setText("Erreur saisissez un chiffre !");
+						monTexte.setText("");
+					}
+				}
+				
+			}
+		});
+		
 		texteSaisi.setColumns(15);
-		texteSaisi.setText("56");
 		pan.add(texteSaisi);
 		this.monTexte = texteSaisi;
 		this.setVisible(true);
 
 	}
 
+	public String calcul(int tm){
+		if(tm < nbAlea)
+			return "C'est plus ";
+		else if (tm > nbAlea)
+			return "C'est moins";
+		else
+			return "Gagné ! ";
+	}
+	
 	public JTextField getNb()
 	{
 		return this.monTexte;
